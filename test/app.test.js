7bbc51a8b3f2ae9f719ecda20b665637897ff705
Rakib <bsse1320@iit.du.ac.bd>
2025-01-07 02:47:18 +0600

    Problem solved

--- test/app.test.js
+++ test/app.test.js
@@ -1,9 +1,9 @@
 const request = require("supertest");
-const app = require("../server");  
+const app = require("../app"); 
 
 describe("GET /", () => {
   it("should return 'Hello, World this is a practice for CICD!'", async () => {
-    const response = await request(app).get("/");
+    const response = await request(app).get("/"); 
     expect(response.statusCode).toBe(200);
     expect(response.text).toBe("Hello, World this is a practice for CICD!");
   });
