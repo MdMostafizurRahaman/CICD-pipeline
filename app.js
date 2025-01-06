9f38b5fa2dffb7d0cc53e6faf7da94619b281683
Rakib <bsse1320@iit.du.ac.bd>
2025-01-07 02:03:23 +0600

    updated

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('CI/CD pipeline learing');
+    res.send('CI/CD pipeline learing, Yeah!!!!!!!!');
 });
 
 module.exports = app;
